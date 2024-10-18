400823dba42e00575c7e42f60d11e15a109ff792
KricsDavid <71211940+KricsDavid@users.noreply.github.com>
2024-10-18 10:47:59 +0200

    Add files via upload

---
+++ src/components/PlayerName.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const PlayerName = () => {
+    const [isPLayerEdit, setPlayerEdit] = useState<boolean>(true);
+
+    const handlePlayerEdit = ()=>{
+      setPlayerEdit(!isPLayerEdit)
+    }
+  return (
+    <div>{isPLayerEdit?
+        (<div onClick={handlePlayerEdit} className="playerName">
+         <h1>Player Name</h1>
+        </div>)
+        :
+        (
+         <form className='playerInput'>
+           <input type="text" name="" id="" />
+           <button type='button'>Save</button>
+         </form>
+        )}</div>
+
+  )
+}
+
+export default PlayerName
